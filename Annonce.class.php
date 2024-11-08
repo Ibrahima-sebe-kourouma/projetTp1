f3cc67b492078d4b598ff2ff1d766f848196e384
Ibrahima-sebe-kourouma <kibse03@gmail.com>
2024-11-07 22:03:33 -0500

    jout de la fontion d'hydratation

--- Annonce.class.php
+++ Annonce.class.php
@@ -64,5 +64,17 @@ class Annonce {
         echo "Auteur: ".$this->auteur."<br>";
         echo "Destinataire: ".$this->destinataire."<br>"; // Affiche le destinataire
     }
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }	
+    
+    }
 }
 ?>
